453d8754c2db51cf086b9d2ba1b100c857c41c96
Yash Raj <36835879+devrra@users.noreply.github.com>
2018-08-19 15:52:00 +0530

    all data can now be accessed

--- mqtt.py
+++ mqtt.py
@@ -5,7 +5,8 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("meena")
 
 def on_message(client, userdata, msg):
-    print(msg.topic +" "+str(msg.payload))
+    mpu_data = eval(msg.payload)
+    print(mpu_data['Ax'],mpu_data['Ay'],mpu_data['Az'],mpu_data['Gx'],mpu_data['Gy'],mpu_data['Gz'])
 
 client = mqtt.Client()
 client.on_message = on_message
@@ -14,4 +15,4 @@ client.on_connect = on_connect
 broker = "broker.mqtt-dashboard.com"
 client.connect(broker,1883,60)
 client.loop_forever()
-    
+
